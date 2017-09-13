c9a6fcc7165e3bc7818ab4e07af4e085cd05cb7f
Ivy Chenyao <ivychenyao@pal-nat186-79-83.itap.purdue.edu>
2017-09-13 09:42:00 -0400

    Add emoji list

--- Emoji Mad Minute/ViewController.swift
+++ Emoji Mad Minute/ViewController.swift
@@ -9,15 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var emojis = [
+        "🏟",
+        "🐫",
+        "⬇️",
+        "⁉️",
+        "🏄",
+        "🏕",
+        "👾",
+        "📪",
+        "🕟",
+        "🚰",
+        "😁",
+        "😈",
+        "😍",
+        "😑",
+        "😛",
+        "😡",
+        "🙅",
+        "🙊",
+        "🙏",
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func declareEmojis() {
+        
+        
+        
     }
 
 

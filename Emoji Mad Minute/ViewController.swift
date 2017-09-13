f52239d013bd1cc111593f89add84c21f76f36ae
Ivy Chenyao <ivychenyao@pal-nat186-79-83.itap.purdue.edu>
2017-09-13 08:49:48 -0400

    Initial Commit

---
+++ Emoji Mad Minute/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Mad Minute
+//
+//  Created by Ivy Chenyao on 9/13/17.
+//  Copyright © 2017 Ivy Chenyao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
